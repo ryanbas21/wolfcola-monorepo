02db102d360ee935b23b7daf29995291f8af95a9
ryan <ryanbas21@gmail.com>
2024-07-30 16:58:24 -0600

    create a shared biome config

--- packages/errors/src/index.ts
+++ packages/errors/src/index.ts
@@ -1,2 +1,2 @@
-export { FetchError } from './lib/fetch-error.js';
-export { ParsingResponseJsonError } from './lib/json-parsing-error.js';
+export { FetchError } from "./lib/fetch-error.js";
+export { ParsingResponseJsonError } from "./lib/json-parsing-error.js";
