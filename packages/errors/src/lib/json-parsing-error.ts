e6f0490fa909f8a81c7ee92a83a8f90ef3758450
Ryan Bas <18267769+ryanbas21@users.noreply.github.com>
2024-08-01 22:23:01 -0600

    Api extractor (#50)

--- packages/errors/src/lib/json-parsing-error.ts
+++ packages/errors/src/lib/json-parsing-error.ts
@@ -1,8 +1,14 @@
+/**
+ * ParsingResponseJsonError
+ * @remarks
+ * Error to throw in effect when failing to parse
+ * a response from fetch calls
+ */
 class ParsingResponseJsonError {
-  constructor(error: unknown) {
-    console.error('parsing', error);
-  }
-  readonly _tag = 'ParsingResponseJsonError';
+	constructor(error: unknown) {
+		console.error("parsing", error);
+	}
+	readonly _tag = "ParsingResponseJsonError";
 }
 
 export { ParsingResponseJsonError };
