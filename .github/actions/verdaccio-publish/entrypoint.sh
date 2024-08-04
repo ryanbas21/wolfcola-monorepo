81c0ddbde50b86c0c95b2e7b9a08167ab7819ad0
ryan <ryanbas21@gmail.com>
2024-08-04 11:02:01 -0600

    add verdaccio and ci publish to verdaccio

--- .github/actions/verdaccio-publish/entrypoint.sh
+++ .github/actions/verdaccio-publish/entrypoint.sh
@@ -6,17 +6,17 @@ local_registry="http://0.0.0.0:4873"
 
 # start local registry
 tmp_registry_log=$(mktemp)
-sh -c "mkdir -p $HOME/.config/verdaccio"
-sh -c "cp --verbose /config.yaml $HOME/.config/verdaccio/config.yaml"
-sh -c "nohup verdaccio --config $HOME/.config/verdaccio/config.yaml &>$tmp_registry_log &"
+sh -c "mkdir -p ${HOME}/.config/verdaccio"
+sh -c "cp --verbose /config.yaml ${HOME}/.config/verdaccio/config.yaml"
+sh -c "nohup verdaccio --config ${HOME}/.config/verdaccio/config.yaml &>${tmp_registry_log} &"
 
 # FIXME: this throws a syntax error, but would be great to make it run
 # grep -q 'http address' <(tail -f $tmp_registry_log)
 # login so we can publish packages
-sh -c "npm-auth-to-token -u test -p test -e test@test.com -r $local_registry"
+sh -c "npm-auth-to-token -u test -p test -e test@test.com -r ${local_registry}"
 
 ## Version the snapshot for ci publishing
-sh -c "pnpm changeset version -- snapshot ci-($PR_NUMBER)"
+sh -c "pnpm changeset version -- snapshot ci-${PR_NUMBER}"
 
 # Run pnpm release command
-sh -c "pnpm changeset publish --registry $local_registry --tag ci-($PR_NUMBER) --no-git-tag"
+sh -c "pnpm changeset publish --registry ${local_registry} --tag ci-${PR_NUMBER} --no-git-tag"
